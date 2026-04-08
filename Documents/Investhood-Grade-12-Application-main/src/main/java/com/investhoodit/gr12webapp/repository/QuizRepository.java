d466369dfa8fa81e1db236fe202c5902b677886f
Mphofela Shupe <dannmphofela@hmail.com>
2026-04-08 13:07:11 +0200

    Code fixed

--- Documents/Investhood-Grade-12-Application-main/src/main/java/com/investhoodit/gr12webapp/repository/QuizRepository.java
+++
@@ -1,9 +0,0 @@
-package com.investhoodit.gr12webapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.investhoodit.gr12webapp.model.Quiz;
-
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-
-}
